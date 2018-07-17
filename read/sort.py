ff92fc2ff3ecc546820628a2b9687196d0b1d0bb
Dan Saunders <djsaunde@umass.edu>
2018-07-17 11:24:25 -0400

    Creating LaTeX generation and updating read tables.

--- read/sort.py
+++ read/sort.py
@@ -5,12 +5,19 @@ import pandas as pd
 
 directories = ['books', 'papers']
 
-for d in directories:
-	f = os.path.join(d, 'read.csv')
 
-	try:
-		df = pd.read_csv(f, parse_dates=['Read'])
-		df = df.sort_values(by='Read', axis='rows', ascending=True)
-		df.to_csv(f, index=False)
-	except pd.errors.EmptyDataError:
-		continue
+def main():
+	for d in directories:
+		print(f'Sorting table in directory {d}/')
+		f = os.path.join(d, 'read.csv')
+
+		try:
+			df = pd.read_csv(f, parse_dates=['Read'])
+			df = df.sort_values(by='Read', axis='rows', ascending=True)
+			df.to_csv(f, index=False)
+		except pd.errors.EmptyDataError:
+			continue
+
+
+if __name__ == '__main__':
+	main()
